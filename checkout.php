fc2136728cf8a013968940c835dc2f7ac12aacf9
Evelyn Eunike Aritonang <117492630+evelynarios@users.noreply.github.com>
2024-05-12 19:31:29 +0700

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,158 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:user_login.php');
+};
+
+if(isset($_POST['order'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $number = $_POST['number'];
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $method = $_POST['method'];
+   $method = filter_var($method, FILTER_SANITIZE_STRING);
+   $address = $_POST['Alamat'] .', '. $_POST['Kota'] .', '. $_POST['Negara'];
+   $address = filter_var($address, FILTER_SANITIZE_STRING);
+   $total_products = $_POST['total_products'];
+   $total_price = $_POST['total_price'];
+
+   $check_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+   $check_cart->execute([$user_id]);
+
+   if($check_cart->rowCount() > 0){
+
+      $insert_order = $conn->prepare("INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price) VALUES(?,?,?,?,?,?,?,?)");
+      $insert_order->execute([$user_id, $name, $number, $email, $method, $address, $total_products, $total_price]);
+
+      $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+      $delete_cart->execute([$user_id]);
+
+      $message[] = 'order placed successfully!';
+   }else{
+      $message[] = 'your cart is empty';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="checkout-orders">
+
+   <form action="" method="POST">
+
+   <h3>your orders</h3>
+
+      <div class="display-orders">
+      <?php
+         $grand_total = 0;
+         $cart_items[] = '';
+         $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+         $select_cart->execute([$user_id]);
+         if($select_cart->rowCount() > 0){
+            while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+               $cart_items[] = $fetch_cart['name'].' ('.$fetch_cart['price'].' x '. $fetch_cart['quantity'].') - ';
+               $total_products = implode($cart_items);
+               $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
+      ?>
+         <p> <?= $fetch_cart['name']; ?> <span>(<?= 'Rp'.$fetch_cart['price'].'/- x '. $fetch_cart['quantity']; ?>)</span> </p>
+      <?php
+            }
+         }else{
+            echo '<p class="empty">your cart is empty!</p>';
+         }
+      ?>
+         <input type="hidden" name="total_products" value="<?= $total_products; ?>">
+         <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
+         <p>grand total : <span>Rp <?= number_format($grand_total, 0, ',', '.'); ?></span></p>
+      </div>
+
+      <h3>place your orders</h3>
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>Nama :</span>
+            <input type="text" name="name" placeholder="masukkan nama" class="box" maxlength="20" required>
+         </div>
+         <div class="inputBox">
+            <span>Nomor Telepon :</span>
+            <input type="Nomor Telepon" name="number" placeholder="masukkan nomor telepon" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+         </div>
+         <div class="inputBox">
+            <span>Email :</span>
+            <input type="Email" name="email" placeholder="masukkan email" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Metode Pembayaran :</span>
+            <select name="method" class="box" required>
+               <option value="cash on delivery">cash on delivery</option>
+               <option value="credit card">credit card</option>
+               <option value="paytm">paytm</option>
+               <option value="paypal">paypal</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>Alamat :</span>
+            <input type="text" name="Alamat" placeholder="e.g. flat number" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Kota :</span>
+            <input type="text" name="Kota" placeholder="e.g. Jakarta" class="box" maxlength="50" required>
+         </div>
+         <div class="inputBox">
+            <span>Negara :</span>
+            <input type="text" name="Negara" placeholder="e.g. Indonesia" class="box" maxlength="50" required>
+         </div>
+      </div>
+
+      <input type="submit" name="order" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>" value="Pesan">
+
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
